930e828402202db24a6669cc372102304adfd2db
Elena Galkina <algalkinaevgen14@gmail.com>
2023-04-11 13:17:47 +0300

    add js

---
+++ script.js
@@ -0,0 +1,20 @@
+function greet()  {
+    return('hello world!);
+  }
+  greet();
+
+  let c = 9;
+  function count () {
+    c++;
+    count();
+  }
+  document.querySelector('.out-1').addEventListener('mousemove', blockWidth);
+let w = 100;
+
+  function blockWidth () {
+    w++;
+    document.querySelector('.out-1').style.width = w + 'px';
+
+    
+  } 
+  
