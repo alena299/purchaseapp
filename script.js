3912d85fba1aad0169434c7a7db5d22e18d4c201
Elena Galkina <algalkinaevgen14@gmail.com>
2023-04-11 13:36:38 +0300

    add info

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ function greet()  {
   let c = 9;
   function count () {
     c++;
-    count();
+    console.log(c);
+   
   }
+  count();
+  
   document.querySelector('.out-1').addEventListener('mousemove', blockWidth);
 let w = 100;
 
   function blockWidth () {
     w++;
     document.querySelector('.out-1').style.width = w + 'px';
-
-    
+   
   } 
   
